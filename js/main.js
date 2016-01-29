177cdd642437091db537937e60c74126c641fab4
UNA A. KRAVETS <uakravet@us.ibm.com>
2016-01-28 23:59:14 -0600

    getting object data in console

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var request = new XMLHttpRequest();
+// var results = require('./results');
+
 // var keys = require('./keybindings');
 // var clicks = require('./clickbindings');
 
@@ -7,10 +9,22 @@ request.open('GET', '/json-list', true);
 request.onload = function() {
   'use strict';
   if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    var getMoment = moments(data);
-    keys(getMoment);
-    clicks(getMoment);
+    var data = JSON.parse(request.responseText).items;
+
+    var results = [];
+
+    for (var i = 0; i < data.length; i++) {
+      results.push({
+        title: data[i].title,
+        image: data[i].link,
+        thumb: data[i].image.thumbnailLink,
+        imageHeight: data[i].image.height,
+        imageWidth: data[i].image.width,
+        link: data[i].image.contextLink
+      });
+    }
+
+    console.log(results);
 
   } else {
     console.log('Something went wrong!');
